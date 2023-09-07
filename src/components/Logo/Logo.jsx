77583bee09a9dc508fea185856b1fcc31b58e169
Sujith Bhat <sujithbhat97@gmail.com>
2023-09-07 23:30:35 +0530

    Session 2: In Session

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import logoImage from "../../assets/logoImage.png";
+
+const Logo = () => {
+  return <img src={logoImage} alt="LogoImage" width={67} />;
+};
+
+export default Logo;
