efe7bfeab8e4b62d949f84d7c0b00309cc4548b2
Sujith Bhat <sujithbhat97@gmail.com>
2023-09-22 20:53:31 +0530

    cardCarouselNewAlbums-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { fetchTopAlbums } from "./api/api";
+import { fetchTopAlbums, fetchNewAlbums } from "./api/api";
 import Hero from "./components/Hero/Hero";
 import NavBar from "./components/NavBar/NavBar";
 import Section from "./components/Section/Section";
 
 function App() {
   const [topAlbumsData, setTopAlbumsData] = useState([]);
+  const [newAlbumsData, setNewAlbumsData] = useState([]);
   const generateData = async () => {
     try {
-      const data = await fetchTopAlbums();
-      setTopAlbumsData(data);
+      const data1 = await fetchTopAlbums();
+      setTopAlbumsData(data1);
+      const data2 = await fetchNewAlbums();
+      setNewAlbumsData(data2);
     } catch (err) {
       console.log(err);
     }
@@ -24,8 +27,11 @@ function App() {
     <div className="App">
       <NavBar />
       <Hero />
-      <div>
+      <div className="sectionWrapper">
         <Section data={topAlbumsData} title="Top Albums" />
+      </div>
+      <div className="sectionWrapper">
+        <Section data={newAlbumsData} title="New Albums" />
       </div>
     </div>
   );
